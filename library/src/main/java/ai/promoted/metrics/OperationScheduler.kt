2db729b1ac9c612d9a6496ea452ffaf10d5cb22e
Drew Dennis <dennisdrew@promoted.ai>
2021-04-28 07:07:25 -0400

    Attempt for Java timer instead of GlobalScope implementation

--- library/src/main/java/ai/promoted/metrics/OperationScheduler.kt
+++ library/src/main/java/ai/promoted/metrics/OperationScheduler.kt
@@ -1,6 +1,8 @@
 package ai.promoted.metrics
 
 import kotlinx.coroutines.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 /**
  * Given an interval, uses a privately managed coroutine scope & job to schedule future execution
@@ -12,20 +14,33 @@ internal class OperationScheduler(
 ) {
     private var job: Job? = null
 
+    private var scheduled = false
+    private val timer = Timer()
+
     /**
      * If the provided operation has not yet been scheduled, schedule it
      */
     fun maybeSchedule() {
-        if (job != null) return
-        else job = scheduleOperation()
+//        if (job != null) return
+//        else job = scheduleOperation()
+        if(scheduled) return
+        timer.schedule(
+            delay = intervalMillis,
+            action = {
+                operation.invoke()
+                scheduled = false
+            }
+        )
     }
 
     /**
      * If the provided operation has been scheduled, cancel the pending execution of the operation
      */
     fun cancel() {
-        job?.cancel()
-        job = null
+//        job?.cancel()
+//        job = null
+        timer.cancel()
+        scheduled = false
     }
 
     private fun scheduleOperation() = GlobalScope.launch {
