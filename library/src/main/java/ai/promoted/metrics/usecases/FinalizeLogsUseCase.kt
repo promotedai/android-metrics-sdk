793428cbf213feb70abda91f682dacb31889c338
Drew Dennis <dennisdrew@promoted.ai>
2021-04-30 19:45:04 -0400

    Proper request headers

--- library/src/main/java/ai/promoted/metrics/usecases/FinalizeLogsUseCase.kt
+++ library/src/main/java/ai/promoted/metrics/usecases/FinalizeLogsUseCase.kt
@@ -4,6 +4,9 @@ import ai.promoted.ClientConfig
 import ai.promoted.networking.PromotedApiRequest
 import com.google.protobuf.Message
 
+private const val HEADER_API_KEY = "x-api-key"
+private const val PROTOBUF_CONTENT_TYPE = "application/protobuf"
+
 internal class FinalizeLogsUseCase(config: ClientConfig) {
     // TODO - dev version
     private val url = config.metricsLoggingUrl
@@ -13,8 +16,19 @@ internal class FinalizeLogsUseCase(config: ClientConfig) {
     fun finalizeLogs(logMessages: List<Message>): PromotedApiRequest {
         return PromotedApiRequest(
             url = url,
-            headers = mapOf(),
+            headers = buildHeaders(),
             bodyData = ByteArray(0)
         )
+    }
+
+    private fun buildHeaders(): Map<String, String> {
+        val headers = mutableMapOf(
+            HEADER_API_KEY to apiKey
+        )
+
+        if(wireFormat == ClientConfig.MetricsLoggingWireFormat.Binary)
+            headers["content-type"] = PROTOBUF_CONTENT_TYPE
+
+        return headers
     }
 }
