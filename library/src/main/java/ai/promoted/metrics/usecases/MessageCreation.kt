dad234eef148aa728a115decf9216636206f49dd
Drew Dennis <dennisdrew@promoted.ai>
2021-05-03 06:39:42 -0400

    Mechanics of tracking action

--- library/src/main/java/ai/promoted/metrics/usecases/MessageCreation.kt
+++ library/src/main/java/ai/promoted/metrics/usecases/MessageCreation.kt
@@ -5,8 +5,11 @@ import ai.promoted.internal.DeviceInfoProvider
 import ai.promoted.internal.PromotedAiLocale
 import ai.promoted.proto.common.Timing
 import ai.promoted.proto.common.UserInfo
+import ai.promoted.proto.event.Action
+import ai.promoted.proto.event.ActionType
 import ai.promoted.proto.event.AppScreenView
 import ai.promoted.proto.event.Device
+import ai.promoted.proto.event.NavigateAction
 import ai.promoted.proto.event.Screen
 import ai.promoted.proto.event.Session
 import ai.promoted.proto.event.Size
@@ -83,6 +86,46 @@ internal fun createViewMessage(
     // TODO - Fill out AppScreenView.
     .setAppScreenView(AppScreenView.getDefaultInstance())
     .build()
+
+internal fun createActionMessage(
+    clock: Clock,
+    name: String,
+    actionId: String,
+    sessionId: String,
+    viewId: String,
+    type: ActionType,
+    impressionId: String?,
+    insertionId: String?,
+    requestId: String?,
+    elementId: String?,
+    targetUrl: String?
+) =
+    Action
+        .newBuilder()
+        .setTiming(createTimingMessage(clock))
+        .setActionId(actionId)
+        .setName(name)
+        .setSessionId(sessionId)
+        .setViewId(viewId)
+        .setActionType(type)
+        .apply {
+            impressionId?.let { setImpressionId(it) }
+            insertionId?.let { setInsertionId(it) }
+            requestId?.let { setRequestId(it) }
+            setElementId(elementId ?: name)
+            if (type == ActionType.NAVIGATE) {
+                navigateAction = createNavigationMessage(targetUrl)
+            }
+        }
+        .build()
+
+private fun createNavigationMessage(targetUrl: String?) =
+    NavigateAction
+        .newBuilder()
+        .apply {
+            targetUrl?.let { setTargetUrl(it) }
+        }
+        .build()
 
 // TODO - when Kotlin 1.5 comes out, use inline/value classes to ensure type-safety
 internal fun createUserInfoMessage(userId: String, logUserId: String) =
---
+++ library/src/main/java/ai/promoted/metrics/usecases/TrackActionUseCase.kt
@@ -0,0 +1,44 @@
+package ai.promoted.metrics.usecases
+
+import ai.promoted.internal.Clock
+import ai.promoted.metrics.MetricsLogger
+import ai.promoted.metrics.id.IdGenerator
+import ai.promoted.proto.event.ActionType
+
+internal class TrackActionUseCase(
+    private val clock: Clock,
+    private val logger: MetricsLogger,
+    private val idGenerator: IdGenerator,
+    private val viewUseCase: TrackViewUseCase,
+    private val sessionUseCase: TrackSessionUseCase
+) {
+    fun onAction(
+        name: String,
+        type: ActionType,
+        contentId: String?,
+        insertionId: String?,
+        requestId: String?,
+        targetUrl: String?,
+        elementId: String?
+    ) {
+        // TODO
+        val actionId = idGenerator.newId()
+        val impressionId = ""
+
+        logger.enqueueMessage(
+            createActionMessage(
+                clock = clock,
+                name = name,
+                actionId = actionId,
+                sessionId = sessionUseCase.sessionId,
+                viewId = viewUseCase.viewId,
+                type = type,
+                impressionId = impressionId,
+                insertionId = insertionId,
+                requestId = requestId,
+                elementId = elementId,
+                targetUrl = targetUrl
+            )
+        )
+    }
+}
