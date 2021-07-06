632a8a5af357c62f4520f89f8732ca560f113c87
Drew Dennis <dennisdrew@promoted.ai>
2021-07-06 16:51:33 -0400

    Cleanup demo app

--- example-app/src/main/java/ai/promoted/MainActivity.kt
+++ example-app/src/main/java/ai/promoted/MainActivity.kt
@@ -1,58 +1,16 @@
 package ai.promoted
 
-import ai.promoted.proto.event.ActionType
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private val javaDelegate = MainActivityJavaDelegate()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (USE_JAVA) return javaDelegate.onCreate(this)
-
-        setupRecyclerView()
-
-        val userId = "User-${System.currentTimeMillis()}"
-        PromotedAi.startSession(userId)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if (USE_JAVA) return javaDelegate.onResume()
-
-        PromotedAi.onViewVisible("MainActivity")
-    }
-
-    private fun setupRecyclerView() {
-        val rv = findViewById<RecyclerView>(R.id.rv)
-        val fakeContent = mutableListOf<AbstractContent.Item>()
-
-        repeat(100) {
-            fakeContent.add(
-                AbstractContent.Item(
-                    name = "Item $it",
-                    insertionId = "insertion-id-$it"
-                )
-            )
-        }
-
-        rv.adapter = Adapter(fakeContent) {
-            PromotedAi.onAction("click-item-${it.name}", ActionType.CUSTOM_ACTION_TYPE) {
-                insertionId = "click-item-insertion-${it.name}"
-            }
-        }
-
-        PromotedAi.trackRecyclerView(
-            recyclerView = rv,
-            currentDataProvider = { fakeContent }
-        ) {
-            percentageThreshold = 50.0
-//            timeThresholdMillis = 3000L
+        findViewById<View>(R.id.sign_in_button).setOnClickListener {
+            SigningInDialog().show(supportFragmentManager, "signing-in")
         }
     }
 }
